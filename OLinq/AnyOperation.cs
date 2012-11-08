c5977e71787f6e7e1050a5db884bf7e9d20186e3
Jerome Haltom <wasabi@larvalstage.net>
2012-11-08 08:19:58 -0800

    * Missing commits.

--- OLinq/AnyOperation.cs
+++ OLinq/AnyOperation.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -11,117 +10,115 @@ namespace OLinq
     class AnyOperation<TSource> : GroupOperation<TSource, bool>
     {
 
-        LambdaExpression predicateExpr;
-        Dictionary<object, LambdaOperation<bool>> predicates = new Dictionary<object, LambdaOperation<bool>>();
+        /// <summary>
+        /// Default predicate expression. Simply returns <c>true</c>.
+        /// </summary>
+        static readonly Expression defaultPredicateExpression =
+            Expression.Lambda<Func<TSource, bool>>(
+                Expression.Constant(true, typeof(bool)),
+                Expression.Parameter(typeof(TSource), "i"));
+
+        LambdaOperationContainer<TSource, bool> predicates;
 
         public AnyOperation(OperationContext context, MethodCallExpression expression)
             : base(context, expression)
         {
-
-        }
-
-        LambdaExpression GetPredicateExpr()
-        {
-            if (predicateExpr != null)
-                return predicateExpr;
-
-            var expr = (MethodCallExpression)Expression;
-            if (expr.Arguments.Count >= 2)
-            {
-                predicateExpr = Utils.UnpackLambda(expr.Arguments[1]);
-                if (predicateExpr == null)
-                    throw new InvalidOperationException("Could not load predicate.");
-            }
-
-            return predicateExpr;
-        }
-
-        protected override void SourceCollectionReset()
-        {
-            base.SourceCollectionReset();
-            ResetValue();
-        }
-
-        protected override void SourceCollectionAddItem(TSource item, int index)
-        {
-            base.SourceCollectionAddItem(item, index);
-
-            // we've added a true item, we must be true
-            if (GetItemValue(item))
-                SetValue(true);
-        }
-
-        protected override void SourceCollectionRemoveItem(TSource item, int index)
-        {
-            base.SourceCollectionRemoveItem(item, index);
-
-            ResetValue();
-        }
-
-        void ResetValue()
-        {
-            SetValue(SourceCollection.Any(i => GetItemValue(i)));
+            SetValue(false);
+            predicates = new LambdaOperationContainer<TSource, bool>(
+                ((MethodCallExpression)Expression).GetArgument(1, defaultPredicateExpression).UnpackLambda<TSource, bool>(),
+                CreatePredicateContext);
+            predicates.CollectionChanged += predicates_CollectionChanged;
+            predicates.LambdaValueChanged += predicates_LambdaValueChanged;
+            predicates.Items = SourceCollection;
         }
 
         /// <summary>
-        /// Invoked when any of the current tests change.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        void predicate_ValueChanged(object sender, ValueChangedEventArgs args)
-        {
-            var oldValue = (bool)args.OldValue;
-            var newValue = (bool)args.NewValue;
-
-            if (oldValue != newValue)
-                if (newValue == true)
-                    // new value is true means 'any' is true
-                    SetValue(true);
-                else if (Value == true)
-                    // new value is false, existing value is true, and so are we
-                    SetValue(true);
-                else
-                    // undetermined, rebuild
-                    ResetValue();
-        }
-
-        private LambdaOperation<bool> GetPredicate(object item)
-        {
-            if (GetPredicateExpr() == null)
-                return null;
-
-            LambdaOperation<bool> predicate;
-            if (!predicates.TryGetValue(item, out predicate))
-            {
-                // generate new parameter
-                var ctx = new OperationContext(Context);
-                var var = OperationFactory.FromValue(item);
-                ctx.Variables[GetPredicateExpr().Parameters[0].Name] = var;
-
-                // create new test and subscribe to test modifications
-                predicate = new LambdaOperation<bool>(ctx, GetPredicateExpr());
-                predicate.Init(); // load before value changed to prevent double notification
-                predicate.ValueChanged += predicate_ValueChanged;
-                predicates[item] = predicate;
-            }
-
-            return predicate;
-        }
-
-        /// <summary>
-        /// Gets whether or not the item is true.
+        /// Creates a context for a new predicate operation.
         /// </summary>
         /// <param name="item"></param>
         /// <returns></returns>
-        private bool GetItemValue(object item)
+        OperationContext CreatePredicateContext(TSource item)
         {
-            var p = GetPredicate(item);
-            return p != null ? p.Value : true;
+            // generate new parameter
+            var ctx = new OperationContext(Context);
+            var var = OperationFactory.FromValue(item);
+            ctx.Variables[predicates.Expression.Parameters[0].Name] = var;
+            return ctx;
+        }
+
+        /// <summary>
+        /// Invoked when the predicate collection changes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void predicates_CollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
+        {
+            switch (args.Action)
+            {
+                case NotifyCollectionChangedAction.Move:
+                case NotifyCollectionChangedAction.Replace:
+                case NotifyCollectionChangedAction.Reset:
+                    ResetValue();
+                    break;
+                case NotifyCollectionChangedAction.Add:
+                    ResetValue();
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    ResetValue();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Invoked when one of the predicates has a value change.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void predicates_LambdaValueChanged(object sender, LambdaValueChangedEventArgs<TSource, bool> args)
+        {
+            if (args.NewValue)
+                SetValue(true);
+            else
+                ResetValue();
+        }
+
+        /// <summary>
+        /// Invoked when the underlying source changes.
+        /// </summary>
+        /// <param name="oldSource"></param>
+        /// <param name="newSource"></param>
+        protected override void SourceChanged(IEnumerable<TSource> oldSource, IEnumerable<TSource> newSource)
+        {
+            base.SourceChanged(oldSource, newSource);
+
+            // update the predicate collection
+            if (predicates != null)
+                predicates.Items = newSource;
+        }
+
+        /// <summary>
+        /// Recalculates the value.
+        /// </summary>
+        void ResetValue()
+        {
+            SetValue(predicates.Any(i => i.Value));
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            // so event gets raised regardless of order of initialization
+            OnValueChanged(null, Value);
         }
 
         public override void Dispose()
         {
-            predicates
+            if (predicates != null)
+            {
+                predicates.Dispose();
+                predicates = null;
+            }
 
             base.Dispose();
         }
