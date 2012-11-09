4b719985488ccac18afc84aa2619f099972733b5
Jerome Haltom <wasabi@larvalstage.net>
2012-11-09 14:21:49 -0800

    Min and Max tests.

---
+++ OLinq.Tests/MaxTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OLinq.Tests
+{
+
+    [TestClass]
+    public class MaxTests
+    {
+
+        [TestMethod]
+        public void MaxTest()
+        {
+            var c = new ObservableCollection<int>()
+            {
+                1,
+                2,
+            };
+
+            var b1 = c.AsObservableQuery()
+                .Observe(i => i.Max());
+            Assert.AreEqual(2, b1.Value);
+
+            c.RemoveAt(1);
+            Assert.AreEqual(1, b1.Value);
+        }
+
+    }
+
+}
---
+++ OLinq.Tests/MinTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OLinq.Tests
+{
+
+    [TestClass]
+    public class MinTests
+    {
+
+        [TestMethod]
+        public void MinTest()
+        {
+            var c = new ObservableCollection<int>()
+            {
+                1,
+                2,
+            };
+
+            var b1 = c.AsObservableQuery()
+                .Observe(i => i.Min());
+            Assert.AreEqual(1, b1.Value);
+
+            c.RemoveAt(0);
+            Assert.AreEqual(2, b1.Value);
+        }
+
+    }
+
+}
