c85024cbab8642c3fabca484106f9db1f055a032
Jerome Haltom <wasabi@larvalstage.net>
2012-11-08 17:26:11 -0800

    Based on common class.

--- OLinq/DistinctOperation.cs
+++ OLinq/DistinctOperation.cs
@@ -7,79 +7,25 @@ using System.Linq.Expressions;
 namespace OLinq
 {
 
-    class DistinctOperation<TElement> : Operation<IEnumerable<TElement>>, IEnumerable<TElement>, INotifyCollectionChanged
+    class DistinctOperation<TElement> : EnumerableSourceOperation<TElement, IEnumerable<TElement>>, IEnumerable<TElement>, INotifyCollectionChanged
     {
-
-        Expression sourceExpr;
-        IOperation<IEnumerable<TElement>> source;
 
         public DistinctOperation(OperationContext context, MethodCallExpression expression)
             : base(context, expression)
         {
-            sourceExpr = expression.Arguments[0];
-
-            source = OperationFactory.FromExpression<IEnumerable<TElement>>(context, sourceExpr);
-            source.ValueChanged += source_ValueChanged;
-        }
-
-        void source_ValueChanged(object sender, ValueChangedEventArgs args)
-        {
-            var oldValue = args.OldValue as INotifyCollectionChanged;
-            if (oldValue != null)
-                oldValue.CollectionChanged -= source_CollectionChanged;
-
-            var newValue = args.NewValue as INotifyCollectionChanged;
-            if (newValue != null)
-                newValue.CollectionChanged += source_CollectionChanged;
-
-            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-        }
-
-        void source_CollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
-        {
-            switch (args.Action)
-            {
-                case NotifyCollectionChangedAction.Reset:
-                case NotifyCollectionChangedAction.Move:
-                case NotifyCollectionChangedAction.Replace:
-                case NotifyCollectionChangedAction.Add:
-                case NotifyCollectionChangedAction.Remove:
-                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-                    break;
-            }
-        }
-
-        IEnumerator<TElement> GetEnumerator()
-        {
-            return source.Value.Distinct().GetEnumerator();
+            SetValue(this);
         }
 
         public override void Init()
         {
-            if (source != null)
-                source.Init();
             base.Init();
 
-            SetValue(this);
+            OnValueChanged(null, Value);
         }
 
-        public override void Dispose()
+        public IEnumerator<TElement> GetEnumerator()
         {
-            if (source != null)
-            {
-                source.ValueChanged -= source_ValueChanged;
-                source.Dispose();
-                var sourceValue = source.Value as INotifyCollectionChanged;
-                if (sourceValue != null)
-                    sourceValue.CollectionChanged -= source_CollectionChanged;
-            }
-
-            base.Dispose();
-        }
-
-        IEnumerator<TElement> IEnumerable<TElement>.GetEnumerator()
-        {
-            return GetEnumerator();
+            return Source.Distinct().GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
