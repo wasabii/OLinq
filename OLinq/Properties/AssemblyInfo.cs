f47e6cb1b51158c0ff06798d25a462b0811223cb
Jerome Haltom <wasabi@larvalstage.net>
2013-06-28 21:30:38 -0500

    Added some build configuration crap to target net40.

--- OLinq/Properties/AssemblyInfo.cs
+++ OLinq/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -10,17 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OLinq")]
-[assembly: AssemblyCopyright("Copyright © Jerome Haltom 2012")]
+[assembly: AssemblyCopyright("Copyright © Jerome Haltom 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aaf6b697-1e96-42b7-8b1e-39b7b725c996")]
 
 // Version information for an assembly consists of the following four values:
 //
