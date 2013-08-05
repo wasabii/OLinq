ad2a1b4933f1a01af30eafa3ec11b2515d02015a
Jerome Haltom <wasabi@larvalstage.net>
2013-08-04 21:56:15 -0500

    Updated version.

--- OLinq/Properties/AssemblyInfo.cs
+++ OLinq/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.13.0")]
-[assembly: AssemblyFileVersion("0.0.13.0")]
+[assembly: AssemblyVersion("0.0.14.0")]
+[assembly: AssemblyFileVersion("0.0.14.0")]
 
 [assembly: InternalsVisibleTo("OLinq.Tests")]
