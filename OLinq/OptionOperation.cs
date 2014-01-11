fab7d3435fa2d475a5c2e312b56979bd18a3ecb0
Jerome Haltom <wasabi@cogito.cx>
2014-01-11 12:49:48 -0600

    Missing files.

---
+++ OLinq/OptionOperation.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+
+namespace OLinq
+{
+
+    /// <summary>
+    /// Sets a context option.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    abstract class OptionOperation<T> :
+        Operation<T>
+    {
+
+        IOperation<T> operation;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="expression"></param>
+        public OptionOperation(OperationContext context, Expression expression)
+            : base(context, expression)
+        {
+            this.operation = OperationFactory.FromExpression<T>(new OperationContext(context), expression);
+            this.operation.ValueChanged += operation_ValueChanged;
+        }
+
+        void operation_ValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            SetValue((T)args.NewValue);
+        }
+
+    }
+
+}
---
+++ OLinq/WithNullSafeOperation.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+
+namespace OLinq
+{
+
+    /// <summary>
+    /// Provides a context option to the graph which enables null-safe operation.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class WithNullSafeOperation<T> :
+        OptionOperation<IQueryable<T>>
+    {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="expression"></param>
+        public WithNullSafeOperation(OperationContext context, MethodCallExpression expression)
+            : base(new OperationContext(context, nullSafe: true), expression.Arguments[0])
+        {
+
+        }
+
+    }
+
+}
