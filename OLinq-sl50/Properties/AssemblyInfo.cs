589a174fabf133b9f62ae2917df39ab3c22de68b
Jerome Haltom <jhaltom@isillc.com>
2014-07-09 13:01:00 -0500

    Added in GitFlowVersion.

--- OLinq-sl50/Properties/AssemblyInfo.cs
+++ OLinq-sl50/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("OLinq")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -12,18 +9,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright © Jerome Haltom 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.14.0")]
-[assembly: AssemblyFileVersion("0.0.14.0")]
 
 [assembly: InternalsVisibleTo("OLinq.Tests")]
